8d809b5614d354aebe6a30e0040bca49dac38a17
SeanCarlson96 <carlsonseanr@gmail.com>
2023-01-12 09:54:02 -0600

    first commit

---
+++ src/main/java/net/yorksolutions/pantrybe/PantryBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.pantrybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantryBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/pantrybe/PantryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.pantrybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
