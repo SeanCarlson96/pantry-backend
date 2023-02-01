18da4cc4f3e30b66aeee701ebdb887057d9ea1d7
SeanCarlson96 <carlsonseanr@gmail.com>
2023-02-01 09:21:36 -0600

    test push

--- src/main/java/net/yorksolutions/pantrybe/PantryBeApplication.java
+++ src/main/java/net/yorksolutions/pantrybe/PantryBeApplication.java
@@ -8,6 +8,7 @@ public class PantryBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PantryBeApplication.class, args);
+        System.out.println("test");
     }
 
 }
